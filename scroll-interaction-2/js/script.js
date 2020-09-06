015473b2723485ffc99a12fb5f5275b5694de22d
hyungju-lee <beegizee1220@gmail.com>
2020-09-06 21:07:09 +0900

    trainning

--- scroll-interaction-2/js/script.js
+++ scroll-interaction-2/js/script.js
@@ -10,6 +10,9 @@
     var enterNewScene = false;
     var rafId;
     var rafState;
+    var canvasHeightRatio;
+    var canvasWidthRatio;
+    var canvasScaleRatio;
 
     var sceneInfo = [
         {
@@ -22,28 +25,38 @@
                 context: document.querySelector('#first-canvas').getContext('2d'),
                 imagesPath: [
                     './image/bg01.jpg',
+                    './image/bg02.jpg',
                 ],
                 images: [],
                 videoPath: [],
                 videoImages: [],
             },
-            canvasRatio: {
-                hRatio: null,
-                wRatio: null,
-                scaleRatio: null,
-            },
             values: {
                 imageSequence: {
-                    // canvas tag and image width 1920 , height 1080 기준
-                    SX: [0, 0, { start: 0, end: 0.33 }],
-                    SY: [0, 0, { start: 0, end: 0.33 }],
-                    SW: [0, 0, { start: 0, end: 0.33 }],
-                    SH: [0, 0, { start: 0, end: 0.33 }],
-                    DX: [0, 0, { start: 0, end: 0.33 }],
-                    DY: [0, 0, { start: 0, end: 0.33 }],
-                    DW: [0, 0, { start: 0, end: 0.33 }],
-                    DH: [0, 0, { start: 0, end: 0.33 }],
+                    first: {
+                        // canvas tag and image width 1920 , height 1080 기준
+                        SX: [0, 0, { start: 0, end: 0.33 }],
+                        SY: [0, 0, { start: 0, end: 0.33 }],
+                        SW: [0, 0, { start: 0, end: 0.33 }],
+                        SH: [0, 0, { start: 0, end: 0.33 }],
+                        DX: [0, 0, { start: 0, end: 0.33 }],
+                        DY: [0, 0, { start: 0, end: 0.33 }],
+                        DW: [0, 0, { start: 0, end: 0.33 }],
+                        DH: [0, 0, { start: 0, end: 0.33 }],
+                    },
+                    second: {
+                        // canvas tag and image width 1920 , height 1080 기준
+                        SX: [0, 0, { start: 0.33, end: 0.66 }],
+                        SY: [0, 0, { start: 0.33, end: 0.66 }],
+                        SW: [0, 0, { start: 0.33, end: 0.66 }],
+                        SH: [0, 0, { start: 0.33, end: 0.66 }],
+                        DX: [0, 0, { start: 0.33, end: 0.66 }],
+                        DY: [0, 0, { start: 0.33, end: 0.66 }],
+                        DW: [0, 0, { start: 0.33, end: 0.66 }],
+                        DH: [0, 0, { start: 0.33, end: 0.66 }],
+                    },
                 },
+                canvasScale: [0, 0, { start: 0.66, end: 1 }],
             },
         },
         {
@@ -97,24 +110,34 @@
     }
 
     var initCanvasValues = function () {
+        // 전체 캔버스 비율 설정
+        canvasHeightRatio = innerHeight / sceneInfo[0].objs.canvas.height;
+        canvasWidthRatio = innerWidth / sceneInfo[0].objs.canvas.width;
+        canvasScaleRatio = canvasHeightRatio > canvasWidthRatio ? canvasHeightRatio : canvasWidthRatio;
+        // 첫번째 씬 첫번째 이미지 설정
         var leftPx = innerWidth < 500 ? innerWidth * 0.1 : innerWidth * 0.15;
-        sceneInfo[0].canvasRatio.hRatio = innerHeight / sceneInfo[0].objs.canvas.height;
-        sceneInfo[0].canvasRatio.wRatio = innerWidth / sceneInfo[0].objs.canvas.width;
-        sceneInfo[0].canvasRatio.scaleRatio = sceneInfo[0].canvasRatio.hRatio > sceneInfo[0].canvasRatio.wRatio ? sceneInfo[0].canvasRatio.hRatio : sceneInfo[0].canvasRatio.wRatio;
-        sceneInfo[0].values.imageSequence.DW[0] = innerWidth * 0.2 < 400? 400 : innerWidth * 0.2;
-        sceneInfo[0].values.imageSequence.DH[0] = innerHeight * 0.15 < 150? 150 : innerHeight * 0.15;
-        sceneInfo[0].values.imageSequence.DW[1] = innerWidth / sceneInfo[0].canvasRatio.scaleRatio;
-        sceneInfo[0].values.imageSequence.DH[1] = innerHeight / sceneInfo[0].canvasRatio.scaleRatio;
-        sceneInfo[0].values.imageSequence.SX[0] = (sceneInfo[0].objs.canvas.width * sceneInfo[0].canvasRatio.scaleRatio) / 2;
-        sceneInfo[0].values.imageSequence.SX[1] = (sceneInfo[0].objs.canvas.width * sceneInfo[0].canvasRatio.scaleRatio - innerWidth) / 2 / sceneInfo[0].canvasRatio.scaleRatio;
-        sceneInfo[0].values.imageSequence.SW[0] = sceneInfo[0].values.imageSequence.DW[0];
-        sceneInfo[0].values.imageSequence.SH[0] = sceneInfo[0].values.imageSequence.DH[0];
-        sceneInfo[0].values.imageSequence.SW[1] = sceneInfo[0].values.imageSequence.DW[1];
-        sceneInfo[0].values.imageSequence.SH[1] = sceneInfo[0].values.imageSequence.DH[1];
-        sceneInfo[0].values.imageSequence.DX[0] = ((sceneInfo[0].objs.canvas.width * sceneInfo[0].canvasRatio.scaleRatio - innerWidth) / 2) / sceneInfo[0].canvasRatio.scaleRatio + leftPx / sceneInfo[0].canvasRatio.scaleRatio;
-        sceneInfo[0].values.imageSequence.DY[0] = sceneInfo[0].objs.canvas.height - sceneInfo[0].values.imageSequence.DH[0] - ((sceneInfo[0].objs.canvas.height * sceneInfo[0].canvasRatio.scaleRatio - innerHeight) / 2) / sceneInfo[0].canvasRatio.scaleRatio;
-        sceneInfo[0].values.imageSequence.DX[1] = ((sceneInfo[0].objs.canvas.width * sceneInfo[0].canvasRatio.scaleRatio - innerWidth) / 2) / sceneInfo[0].canvasRatio.scaleRatio;
-        sceneInfo[0].values.imageSequence.DY[1] = ((sceneInfo[0].objs.canvas.height * sceneInfo[0].canvasRatio.scaleRatio - innerHeight) / 2) / sceneInfo[0].canvasRatio.scaleRatio;
+        sceneInfo[0].values.imageSequence.first.SW[0] = sceneInfo[0].values.imageSequence.first.DW[0] = innerWidth * 0.2 < 400? 400 : innerWidth * 0.2;
+        sceneInfo[0].values.imageSequence.first.SW[1] = sceneInfo[0].values.imageSequence.first.DW[1] = innerWidth / canvasScaleRatio;
+        sceneInfo[0].values.imageSequence.first.SH[0] = sceneInfo[0].values.imageSequence.first.DH[0] = innerHeight * 0.15 < 150? 150 : innerHeight * 0.15;
+        sceneInfo[0].values.imageSequence.first.SH[1] = sceneInfo[0].values.imageSequence.first.DH[1] = innerHeight / canvasScaleRatio;
+        sceneInfo[0].values.imageSequence.first.SX[1] = sceneInfo[0].values.imageSequence.first.DX[1] = (sceneInfo[0].objs.canvas.width * canvasScaleRatio - innerWidth) / 2 / canvasScaleRatio;
+        sceneInfo[0].values.imageSequence.first.SX[0] = (sceneInfo[0].objs.canvas.width * canvasScaleRatio) / 2;
+        sceneInfo[0].values.imageSequence.first.DX[0] = ((sceneInfo[0].objs.canvas.width * canvasScaleRatio - innerWidth) / 2) / canvasScaleRatio + leftPx / canvasScaleRatio;
+        sceneInfo[0].values.imageSequence.first.DY[0] = sceneInfo[0].objs.canvas.height - sceneInfo[0].values.imageSequence.first.DH[0] - (sceneInfo[0].objs.canvas.height * canvasScaleRatio - innerHeight) / 2 / canvasScaleRatio;
+        sceneInfo[0].values.imageSequence.first.DY[1] = ((sceneInfo[0].objs.canvas.height * canvasScaleRatio - innerHeight) / 2) / canvasScaleRatio;
+        // 첫번째 씬 두번째 이미지 설정
+        sceneInfo[0].values.canvasScale[0] = canvasScaleRatio;
+        sceneInfo[0].values.canvasScale[1] = innerHeight / innerWidth > 1 ? canvasScaleRatio * 0.2 : canvasScaleRatio * 0.4;
+        sceneInfo[0].values.imageSequence.second.SX[0] = sceneInfo[0].values.imageSequence.second.DX[0] = (sceneInfo[0].objs.canvas.width * canvasScaleRatio - innerWidth) / 2 / canvasScaleRatio;
+        sceneInfo[0].values.imageSequence.second.SX[1] = sceneInfo[0].values.imageSequence.second.DX[1] = 0;
+        sceneInfo[0].values.imageSequence.second.SY[0] = sceneInfo[0].values.imageSequence.second.SH[1] =
+            sceneInfo[0].values.imageSequence.second.DY[0] = sceneInfo[0].values.imageSequence.second.DH[1] =
+                ((sceneInfo[0].objs.canvas.height * canvasScaleRatio - innerHeight) / 2 + innerHeight) / canvasScaleRatio;
+        sceneInfo[0].values.imageSequence.second.SY[1] = sceneInfo[0].values.imageSequence.second.SH[0] =
+            sceneInfo[0].values.imageSequence.second.DY[1] = sceneInfo[0].values.imageSequence.second.DH[0] =
+                ((sceneInfo[0].objs.canvas.height * canvasScaleRatio - innerHeight) / 2) / canvasScaleRatio;
+        sceneInfo[0].values.imageSequence.second.SW[0] = sceneInfo[0].values.imageSequence.second.DW[0] =((sceneInfo[0].objs.canvas.width * canvasScaleRatio - innerWidth) / 2 + innerWidth) / canvasScaleRatio;
+        sceneInfo[0].values.imageSequence.second.SW[1] = sceneInfo[0].values.imageSequence.second.DW[1] = 1920;
     }
 
     var currentSection = function () {
@@ -211,20 +234,30 @@
         switch (currentScene) {
             case 0:
                 sceneInfo[0].objs.context.clearRect(0, 0, 1920, 1080);
-                var sx = calcValues(sceneInfo[0].values.imageSequence.SX);
-                var sy = calcValues(sceneInfo[0].values.imageSequence.SY);
-                var sw = calcValues(sceneInfo[0].values.imageSequence.SW);
-                var sh = calcValues(sceneInfo[0].values.imageSequence.SH);
-                var dx = calcValues(sceneInfo[0].values.imageSequence.DX);
-                var dy = calcValues(sceneInfo[0].values.imageSequence.DY);
-                var dw = calcValues(sceneInfo[0].values.imageSequence.DW);
-                var dh = calcValues(sceneInfo[0].values.imageSequence.DH);
+                var sx = calcValues(sceneInfo[0].values.imageSequence.first.SX);
+                var sy = calcValues(sceneInfo[0].values.imageSequence.first.SY);
+                var sw = calcValues(sceneInfo[0].values.imageSequence.first.SW);
+                var sh = calcValues(sceneInfo[0].values.imageSequence.first.SH);
+                var dx = calcValues(sceneInfo[0].values.imageSequence.first.DX);
+                var dy = calcValues(sceneInfo[0].values.imageSequence.first.DY);
+                var dw = calcValues(sceneInfo[0].values.imageSequence.first.DW);
+                var dh = calcValues(sceneInfo[0].values.imageSequence.first.DH);
+                var sx2 = calcValues(sceneInfo[0].values.imageSequence.second.SX);
+                var sy2 = calcValues(sceneInfo[0].values.imageSequence.second.SY);
+                var sw2 = calcValues(sceneInfo[0].values.imageSequence.second.SW);
+                var sh2 = calcValues(sceneInfo[0].values.imageSequence.second.SH);
+                var dx2 = calcValues(sceneInfo[0].values.imageSequence.second.DX);
+                var dy2 = calcValues(sceneInfo[0].values.imageSequence.second.DY);
+                var dw2 = calcValues(sceneInfo[0].values.imageSequence.second.DW);
+                var dh2 = calcValues(sceneInfo[0].values.imageSequence.second.DH);
+                var canvasScaleNum = calcValues(sceneInfo[0].values.canvasScale);
                 sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.images[0], sx, sy, sw, sh, dx, dy, dw, dh);
-                sceneInfo[0].objs.canvas.style.transform = 'translate3d(-50%,-50%,0) scale('+ sceneInfo[0].canvasRatio.scaleRatio +')';
+                sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.images[1], sx2, sy2, sw2, sh2, dx2, dy2, dw2, dh2);
+                sceneInfo[0].objs.canvas.style.transform = 'translate3d(-50%,-50%,0) scale('+ canvasScaleNum +')';
                 break;
             case 1:
-                var vNum = Math.round(calcValues(sceneInfo[1].values.videoSequence));
-                sceneInfo[1].objs.context.drawImage(sceneInfo[1].objs.videoImages[vNum], 0, 0);
+                // var vNum = Math.round(calcValues(sceneInfo[1].values.videoSequence));
+                // sceneInfo[1].objs.context.drawImage(sceneInfo[1].objs.videoImages[vNum], 0, 0);
 
 
 

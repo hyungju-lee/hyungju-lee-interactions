f7f5ee00e55401ba5203ebda0343a235c00117c7
hyungju-lee <beegizee1220@gmail.com>
2020-09-08 06:18:54 +0900

    loading 화면 수정

--- scroll-interaction-2/js/script.js
+++ scroll-interaction-2/js/script.js
@@ -12,7 +12,9 @@
         rafState,
         canvasHeightRatio,
         canvasWidthRatio,
-        canvasScaleRatio;
+        canvasScaleRatio,
+        totalImages = 632,
+        loadedImages = 0;
 
     var sceneInfo = [
         {
@@ -89,6 +91,9 @@
         var imgElem;
         for (var i = 0; i < sceneInfo.length; i++) {
             for (var j = 0; j < sceneInfo[i].objs.imagesPath.length; j++) {
+                loadedImages += 1;
+                document.querySelector('.percent').innerText = Math.round(loadedImages / totalImages * 100);
+
                 imgElem = new Image();
                 imgElem.src = sceneInfo[i].objs.imagesPath[j];
                 sceneInfo[i].objs.images.push(imgElem);
@@ -100,6 +105,9 @@
         var videoElem;
         for (var i = 0; i < sceneInfo.length; i++) {
             for (var j = 1; j < sceneInfo[i].values.videoImageCount + 1; j++) {
+                loadedImages += 1;
+                document.querySelector('.percent').innerText = Math.round(loadedImages / totalImages * 100);
+
                 var videoElem = new Image();
                 var num = j < 10? '0000' + j : j < 100? '000' + j : j < 1000? '00' + j :'0' + j;
                 videoElem.src = sceneInfo[i].objs.videoPath + 'scene' + num +'.jpg';
